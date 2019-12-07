c66d60d7a77d7dc463ca53034bb56a041eaf8311
Neville Omangi <nevomangi@gmail.com>
2019-12-07 18:40:21 +0300

    set up pickr

---
+++ src/utils/pickr-opts.js
@@ -0,0 +1,24 @@
+export default {
+  theme: 'nano',
+  closeOnScroll: true,
+  swatches: null,
+  defaultRepresentation: 'RGBA',
+  adjustableNumbers: true,
+  useAsButton: true,
+  components: {
+    preview: true,
+    opacity: true,
+    hue: true,
+    
+    interaction: {
+      hex: true,
+      rgba: true,
+      hsla: true,
+      input: true,
+      save: true,
+    },
+  },
+  strings: {
+    save: 'Save',
+  }
+}
